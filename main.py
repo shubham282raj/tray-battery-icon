c332b9683a7a6fbbe8811c520ba7b839086d271d
Shubham Raj <shubham282raj@gmail.com>
2024-06-08 14:04:49 +0530

    Added auto sized number display on tray icon

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import pystray
 from utils.tray_icon import create_tray_icon
 from utils.tray_menu import menu
 
-icon = pystray.Icon("battery", create_tray_icon(), "Battery Icon", menu)
+icon = pystray.Icon("battery", create_tray_icon(17), "Battery Icon", menu)
 icon.run()
--- utils/tray_icon.py
+++ utils/tray_icon.py
@@ -1,8 +1,31 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
-def create_tray_icon():
+def get_max_font_size(text, max_width, max_height):
+    size = 1
+    font = ImageFont.load_default(size)
+
+    width, height = 0, 0
+    
+    while width < max_width and height < max_height:
+        left, top, right, bottom = font.getbbox(text)
+        width = right - left
+        height = top - bottom
+        size += 1
+        font = ImageFont.load_default(size)
+        
+    return size - 1
+
+def create_tray_icon(number):
     width = 32
     height = 32
+    text = str(number)
     image = Image.new('RGBA', (width, height), (255, 255, 255, 1))
+    
+    # draw on the image created
+    draw = ImageDraw.Draw(image)
+
+    max_font_size = get_max_font_size(text, width, height)
+
+    draw.text((0,0), text, font=ImageFont.load_default(size=max_font_size), fill='white')
 
     return image
