42314cfedb1f44458bd4131b2345a17749d50da5
Shubham Raj <shubham282raj@gmail.com>
2024-06-08 14:54:20 +0530

    Optimized icon updation on battery change only

--- utils/battery.py
+++ utils/battery.py
@@ -7,7 +7,13 @@ def get_battery_percentage():
     return battery.percent
 
 def update_icon(icon):
+    last_battery_percentage = get_battery_percentage()
+    icon.icon = create_tray_icon(last_battery_percentage)
     while True:
         battery_percentage = get_battery_percentage()
-        icon.icon = create_tray_icon(battery_percentage)
-        time.sleep(30)
+        if last_battery_percentage != battery_percentage:
+          icon.icon = create_tray_icon(battery_percentage)
+          last_battery_percentage = battery_percentage
+          time.sleep(30)
+        else:
+          time.sleep(10)
